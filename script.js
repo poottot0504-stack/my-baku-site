635d2c7d07e4772efb8aeb87a2dbbd988bb5d188
poottot0504-stack <poot.tot.0504@gmail.com>
2026-06-06 17:10:34 +0900

    fix conflict

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function resetCart() {
-  cartCount = 0;
-  localStorage.setItem("cartCount", cartCount);
-  document.getElementById("cart-count").textContent = cartCount;
+  if (confirm("カートを空にしますか？")) {
+    cartCount = 0;
+    localStorage.setItem("cartCount", cartCount);
+    document.getElementById("cart-count").textContent = cartCount;
+  }
 }
